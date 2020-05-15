4744b53a0e025ac3058acb7dc8127f4382c65a52
Antonio Román <kyradiscord@gmail.com>
2020-05-15 20:22:53 +0200

    feat(*): added ProxyCache
    This will serve for GuildMember#roles and possibly more things!

---
+++ test/ProxyCache.ts
@@ -0,0 +1,110 @@
+import ava from 'ava';
+import { ProxyCache } from '../dist';
+
+const cache = new Map([['first', 'foo'], ['second', 'bar'], ['third', 'baz']]);
+const proxy = new ProxyCache(cache, ['first', 'second']);
+const emptyProxy = new ProxyCache(cache);
+
+// size
+
+ava('get the size of an empty proxy', (test): void => {
+	test.is(emptyProxy.size, 0);
+});
+
+ava('get the size of a filled proxy', (test): void => {
+	test.is(proxy.size, 2);
+});
+
+// get
+
+ava('get a non-existing key from the cache', (test): void => {
+	test.is(proxy.get('fourth'), undefined);
+});
+
+ava('get a non-existing key from the proxy', (test): void => {
+	test.is(proxy.get('third'), undefined);
+});
+
+ava('get an existing key from the proxy', (test): void => {
+	test.is(proxy.get('second'), 'bar');
+});
+
+// has
+
+ava('get whether or not a non-existing key from the cache exists', (test): void => {
+	test.is(proxy.has('fourth'), false);
+});
+
+ava('get whether or not a non-existing key from the proxy exists', (test): void => {
+	test.is(proxy.has('third'), false);
+});
+
+ava('get whether or not an existing key from the proxy exists', (test): void => {
+	test.is(proxy.has('second'), true);
+});
+
+// set
+
+ava('set a non-existing key from the cache', (test): void => {
+	test.plan(2);
+
+	const clone = proxy.clone();
+	test.is(clone.set('fourth').size, 2);
+	test.is(clone.get('fourth'), undefined);
+});
+
+ava('set a non-existing key from the proxy', (test): void => {
+	test.plan(2);
+
+	const clone = proxy.clone();
+	test.is(clone.set('third').size, 3);
+	test.is(clone.get('third'), 'baz');
+});
+
+ava('set an existing key from the proxy', (test): void => {
+	test.plan(2);
+
+	const clone = proxy.clone();
+	test.is(clone.set('second').size, 2);
+	test.is(clone.get('second'), 'bar');
+});
+
+// delete
+
+ava('delete a non-existing key from the cache', (test): void => {
+	test.plan(3);
+
+	const clone = proxy.clone();
+	test.is(clone.delete('fourth'), false);
+	test.is(clone.get('fourth'), undefined);
+	test.is(clone.size, 2);
+});
+
+ava('delete a non-existing key from the proxy', (test): void => {
+	test.plan(3);
+
+	const clone = proxy.clone();
+	test.is(clone.delete('third'), false);
+	test.is(clone.get('third'), undefined);
+	test.is(clone.size, 2);
+});
+
+ava('delete an existing key from the proxy', (test): void => {
+	test.plan(3);
+
+	const clone = proxy.clone();
+	test.is(clone.delete('second'), true);
+	test.is(clone.get('second'), undefined);
+	test.is(clone.size, 1);
+});
+
+// clear
+
+ava('clear a proxy', (test): void => {
+	test.plan(3);
+
+	const clone = proxy.clone();
+	test.is(clone.clear(), clone);
+	test.is(clone.get('second'), undefined);
+	test.is(clone.size, 0);
+});
